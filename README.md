db5f609aa7c5257ac35160508a0afab663c235a5
Kardo <kardoj@gmail.com>
2017-06-14 19:24:53 +0300

    Initial

---
+++ README.md
@@ -0,0 +1,4 @@
+### Eon test
+A separate project and repository for testing [Eon](https://github.com/kardoj/eon).
+
+All Eon files must be included to the project before building and executing the program.
---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "eon.h"
+#include "date.h"
+
+using namespace std;
+
+int main()
+{
+    // This can be used as a reference on how to pass params to the Eon object
+    int argc = 1;
+    char a1[] = { "eon " };
+    char a2[] = { "" };
+    char *argv[] = { a1, a2 };
+
+    Eon eon = Eon(argc, argv);
+    return 0;
+}
