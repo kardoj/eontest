aa17c273c2bae32f75df3d72195d50028280aee0
Kardo <kardoj@gmail.com>
2017-08-21 20:27:36 +0300

    Fixes a couple of tests after removing projects id file and adding project use count

--- src/projecttest.cpp
+++ src/projecttest.cpp
@@ -15,7 +15,6 @@ void ProjectTest::test()
     cout << "=== ProjectTest ===" << endl;
     adds_new_project();
     fails_adding_a_project_if_projects_file_fails();
-    fails_adding_a_project_if_projects_id_file_fails();
     project_does_not_exist();
     project_exists();
     cout << endl;
@@ -31,8 +30,8 @@ void ProjectTest::adds_new_project()
     Tree().init(datetime, messages_human);
     char name[] = { "new project" };
     Project().add(name, datetime, messages_human);
-    string expected_str = "1 \"" + string(Project::DEFAULT_PROJECT_NAME) + "\" " + datetime + " " + datetime + "\n" +
-                          "2 \"" + name + "\" " + datetime + " " + datetime + "\n";
+    string expected_str = "1 \"" + string(Project::DEFAULT_PROJECT_NAME) + "\" 0 " + datetime + " " + datetime + "\n" +
+                          "2 \"" + name + "\" 0 " + datetime + " " + datetime + "\n";
     assert_file_contents_equal(expected_str, Tree::PROJECTS_FILE, MAX_ROW_LENGTH_DEFAULT);
     assert_equal(Project::msg_project_added(name), messages_human.at(1));
 }
@@ -59,30 +58,6 @@ void ProjectTest::fails_adding_a_project_if_projects_file_fails()
         FailingProjectsFileProject().add(name, datetime, messages_human), "FailingProjectsFileProject().add()"
     );
     assert_equal(Project::MSG_ERROR_OPENING_PROJECTS_FILE, messages_human.at(1));
-}
-
-void ProjectTest::fails_adding_a_project_if_projects_id_file_fails()
-{
-    cout << "TEST " << __FUNCTION__ << endl;
-    remove_dir_recursively(Tree::ROOT_DIR);
-
-    string datetime = Date::current_date_with_time();
-    vector<string> messages_human;
-    Tree().init(datetime, messages_human);
-    char name[] = { "new project" };
-
-    class FailingProjectsIdFileProject : public Project
-    {
-        const char *projects_id_file()
-        {
-            return "./some/nonexistent/path";
-        }
-    };
-
-    assert_false(
-        FailingProjectsIdFileProject().add(name, datetime, messages_human), "FailingProjectsIdFileProject().add()"
-    );
-    assert_equal(Project::MSG_ERROR_OPENING_ID_FILE, messages_human.at(1));
 }
 
 void ProjectTest::project_does_not_exist()
